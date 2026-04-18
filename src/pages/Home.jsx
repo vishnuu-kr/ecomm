f591e9bd8fd12b7644de52059aa3549e39c350e3
vishnuu-kr <vishnu.kr.7674@gmail.com>
2026-04-18 19:59:37 +0530

    Update Home.jsx and related components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ArrowRight } from 'lucide-react';
 import { products, categories } from '../data/products';
